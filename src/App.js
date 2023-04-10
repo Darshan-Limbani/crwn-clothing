cd5859f5bcd65bc7cb2a9aa1d9c40d84c9955e32
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-10 16:03:55 +0530

    Persisting the Navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Route, Routes} from "react-router-dom";
 import './App.css';
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
 
 const Shop = () => {
@@ -12,7 +13,8 @@ const App = () => {
 
     return (
         <Routes>
-            <Route path={'/home'} element={<Home/>}>
+            <Route path={'/'} element={<Navigation/>}>
+                <Route index element={<Home/>}/>
                 <Route path={'shop'} element={<Shop/>}/>
             </Route>
         </Routes>
