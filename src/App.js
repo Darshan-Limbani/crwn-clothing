c352e77cc7f242d7713ea4acc657062b7b03f0ae
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-10 15:41:46 +0530

    Nested Routing and Outlet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import {Route, Routes} from "react-router-dom";
 import './App.css';
 import Home from "./routes/home/home.component";
 
+
+const Shop = () => {
+    return <h1>I am the Shop Page</h1>;
+};
+
+
 const App = () => {
 
     return (
         <Routes>
             <Route path={'/home'} element={<Home/>}>
-
+                <Route path={'shop'} element={<Shop/>}/>
             </Route>
         </Routes>
     );
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,3 +1,4 @@
+import {Outlet} from "react-router-dom";
 import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
@@ -31,8 +32,10 @@ const Home = () => {
     ];
 
     return (
-        <Directory categories={categories}/>
-    );
+        <div>
+            <Outlet/>
+            <Directory categories={categories}/>
+        </div>);
 };
 
 export default Home;
