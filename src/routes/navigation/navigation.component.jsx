b809ae6b3c5078d40368afd667cfef65436ed035
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-11 20:29:34 +0530

    Added Product-Card Component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import {Fragment, useContext} from "react";
 import {Link, Outlet} from "react-router-dom";
 
 import {ReactComponent as CrwnLogo} from "../../assets/logo.svg";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 import {UserContext} from "../../context/user.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
 import './navigation.styles.scss';
@@ -23,8 +24,9 @@ const Navigation = () => {
                     <Link to={'/auth'} className={'nav-link'}>
                         SIGN IN
                     </Link>)}
-
+                <CartIcon/>
             </div>
+
         </div>
         <Outlet/>
     </Fragment>);
