1ef1cd1fe34965dbc986ccb150a16c2160316d89
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-12 20:32:31 +0530

    Added Redirect from home page

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,16 +1,22 @@
+import {useNavigate} from "react-router-dom";
 import './directory-item.styles.scss';
 
 const CategoryItem = ({category}) => {
-    const {title, imageUrl} = category;
-    return (
-        <div className="directory-item-container">
-            <div className={'background-image'} style={{backgroundImage: `url(${imageUrl})`}}/>
-            <div className="body">
-                <h2>{title}</h2>
-                <p>Shop Now</p>
-            </div>
+    const {title, imageUrl, route} = category;
+
+    const navigate = useNavigate();
+
+    function onNavigateHandler() {
+        navigate(route);
+    }
+
+    return (<div className="directory-item-container" onClick={onNavigateHandler}>
+        <div className={'background-image'} style={{backgroundImage: `url(${imageUrl})`}}/>
+        <div className="body">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
         </div>
-    );
+    </div>);
 };
 
 export default CategoryItem;
