96d13b3c93455f9a100b732a10aa55b982ec1ab0
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-14 19:31:44 +0530

    Persisting Redux Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
 import {applyMiddleware, compose, createStore} from "redux";
 import {logger} from "redux-logger/src";
-import {persistStore, persistReducer} from 'redux-persist';
+import {persistReducer, persistStore} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import {rootReducer} from "./root-reducer";
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
 
 const persistConfig = {
     key: 'root',
@@ -14,7 +14,11 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer = (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSTION_COMPOSE__) || compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
